ad3f11721aa42381bb77da31234aea84c463d1f7
Jay Madden <25359755+Jay-Madden@users.noreply.github.com>
2024-08-17 19:13:16 -0500

    feat: add support for named returns

--- lua/auto-fix-return/lib.lua
+++ lua/auto-fix-return/lib.lua
@@ -13,17 +13,33 @@ M.wrap_golang_return = function()
     return
   end
 
+  vim.print("line: " .. vim.api.nvim_get_current_line())
+
+  -- This query attempts to match all valid and also most common invalid or inprogress syntax trees for a function declaration
+  -- short_var_declaration is for the edge case of named returns
+  -- EXAMPLE: func foo() err error { }
   local query_str = [[
-    (_
-       result: (_) @result 
-       (ERROR)? @error 
-    ) 
+  [
+      (_
+           (ERROR)? @error_start 
+           result: (_) @result 
+           (ERROR)? @error_end
+      ) 
+      (short_var_declaration 
+          left: (expression_list
+              (identifier) @named_result
+              (ERROR (identifier) @error_end)?
+          )
+      )
+  ]
   ]]
 
   local query = vim.treesitter.query.parse("go", query_str)
   local cursor_row, cursor_col = unpack(vim.api.nvim_win_get_cursor(0))
 
-  local tree = vim.treesitter.get_node():tree()
+  -- We make sure to call the entire parse again to make sure we have the most up to date tree
+  -- NOTE: without this the bugs are a bit nasty
+  local tree = vim.treesitter.get_parser(0):parse(true)[1]
 
   local final_start_row, final_start_col, final_end_row, final_end_col = 0, 0, 0, 0
 
@@ -35,10 +51,19 @@ M.wrap_golang_return = function()
     end
 
     local capture_name = query.captures[id]
-    if capture_name == "result" then
+
+    -- If we find a start error then we know we are possibly doing a named return
+    if capture_name == "error_start" then
       final_start_row, final_start_col, final_end_row, final_end_col = node:range()
-    end
-    if capture_name == "error" then
+    elseif capture_name == "result" and final_end_row == 0 then
+      final_start_row, final_start_col, final_end_row, final_end_col = node:range()
+    elseif capture_name == "named_result" then
+      final_end_col = end_col + 1
+      final_end_row = end_row
+    elseif capture_name == "result" and final_end_row ~= 0 then
+      final_end_col = end_col
+      final_end_row = end_row
+    elseif capture_name == "error_end" then
       final_end_col = end_col
       final_end_row = end_row
     end
@@ -54,6 +79,7 @@ M.wrap_golang_return = function()
     final_end_col,
     {}
   )[1]
+
   if line == "" then
     return
   end
@@ -68,8 +94,26 @@ M.wrap_golang_return = function()
   -- grab the current cusor position so we can adjust it below
   local final_cursor_col = cursor_col
 
+  -- If there are any commas in the return definition we know we will need parenthesis
   local returns = vim.split(value, ",")
+
+  local function trim_end(s)
+    return s:gsub("%s+$", "")
+  end
+  -- If we do not have any commas we might still be doing a named return
+  -- `E.G func foo() err e` <- once the e is typed we know a named return has been
+  -- initiated and we should split it again,
+  -- however, we need to trim the leading space so we dont surround the return after you have hit space with
+  -- JUST a type return
+  if #returns == 1 then
+    local trimmed = trim_end(value)
+    returns = vim.split(trimmed, " ")
+  end
+
   local new_line = line
+
+  -- If returns just equals one we know we have a single return and do
+  -- not need parenthesis
   if #returns == 1 then
     final_cursor_col = final_cursor_col - 1
     new_line = value
@@ -91,6 +135,7 @@ M.wrap_golang_return = function()
     final_end_col,
     { new_line }
   )
+
   vim.api.nvim_win_set_cursor(0, { final_end_row + 1, final_cursor_col })
 end
 
